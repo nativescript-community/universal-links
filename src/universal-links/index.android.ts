b7f572a22f309b90ae9b8c8af76c4e60da581eaa
Dimitris - Rafail Katsampas <katsampasdr@gmail.com>
2022-04-12 23:13:18 +0000

    fix: Delete intent data on activity destroy as previous method caused issues on startup.

--- src/universal-links/index.android.ts
+++ src/universal-links/index.android.ts
@@ -6,18 +6,26 @@ Application.android.on(AndroidApplication.activityNewIntentEvent, (args) => {
     const intent: android.content.Intent = args.activity.getIntent();
     try {
         const data = intent.getData();
-        if (data) {
-            setUniversalLink(data.toString());
-            intent.setData(null);
+        if (!data) {
+            return; // nothing to do
+        }
 
-            const callback = getRegisteredCallback();
-            if (callback) {
-                callback(getUniversalLink());
-            }
-        } else {
-            setUniversalLink();
+        setUniversalLink(data.toString());
+        const callback = getRegisteredCallback();
+        if (callback) {
+            callback(getUniversalLink());
         }
     } catch (e) {
         console.error(e);
+    }
+});
+
+Application.android.on(AndroidApplication.activityDestroyedEvent, (args) => {
+    const intent: android.content.Intent = args.activity.getIntent();
+    const data = intent.getData();
+
+    if (data) {
+        intent.setData(null);
+        setUniversalLink();
     }
 });
