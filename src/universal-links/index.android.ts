3c2baf99761d4a11392bbd5b0be1625f7c470062
Dimitris - Rafail Katsampas <katsampasdr@gmail.com>
2022-04-12 21:42:21 +0000

    fix(android): Reset open URL intent on app restart.

--- src/universal-links/index.android.ts
+++ src/universal-links/index.android.ts
@@ -6,11 +6,17 @@ Application.android.on(AndroidApplication.activityNewIntentEvent, (args) => {
     const intent: android.content.Intent = args.activity.getIntent();
     try {
         const data = intent.getData();
-        if (!data) return; // nothing to do
+        if (data) {
+            setUniversalLink(data.toString());
+            intent.setData(null);
 
-        setUniversalLink(data.toString());
-        const callback = getRegisteredCallback();
-        if (callback) callback(getUniversalLink());
+            const callback = getRegisteredCallback();
+            if (callback) {
+                callback(getUniversalLink());
+            }
+        } else {
+            setUniversalLink();
+        }
     } catch (e) {
         console.error(e);
     }
