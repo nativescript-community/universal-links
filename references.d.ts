b58b83ff4d32976478d6f72ab02553910233ddd5
Martin Guillon <dev@akylas.fr>
2023-05-31 11:00:02 +0200

    chore: references fix

---
+++ references.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="./node_modules/@nativescript/types-ios/lib/ios/objc-x86_64/objc!Foundation.d.ts" />
+/// <reference path="./node_modules/@nativescript/types-ios/lib/ios/objc-x86_64/objc!UIKit.d.ts" />
+/// <reference path="./node_modules/@nativescript/types-android/lib/android-32.d.ts" />
+/// <reference path="./node_modules/@nativescript/core/global-types.d.ts" />
